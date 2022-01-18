471b96c44d122efaa63d733ab94b31be6085f579
Valerie Bailey <v@valbailey.com>
2022-01-18 15:31:48 +0000

    Display random joke

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,30 @@
+import type { LoaderFunction } from "remix";
+import { useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  randomJoke: { content: string };
+};
+
+export const loader: LoaderFunction = async (): Promise<LoaderData> => {
+  const count = await db.joke.count();
+  const randomRow = Math.floor(Math.random() * count);
+
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    select: { content: true },
+    skip: randomRow,
+  });
+  return { randomJoke };
+};
+
 export default function JokesIndexRoute() {
+  const { randomJoke } = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{randomJoke.content}</p>
     </div>
   );
 }
